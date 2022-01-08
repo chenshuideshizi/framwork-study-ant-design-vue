63e43ea0a966b125946336d16601e86fd559c352
chenshuideshizi <fanwenxiang326@163.com>
2022-01-09 00:10:24 +0800

    edit

--- site/vite.config.js
+++ site/vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue';
-// import md from '../plugin/md';
-import docs from '../plugin/docs/index.js';
-import Inspect from 'vite-plugin-inspect'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    Inspect(), // only applies in dev mode
-    docs(),
-    // md(),
-    vue({include: [/\.vue$/, /\.md$/],})
+    vue()
   ],
   server: {
     port: 3004
